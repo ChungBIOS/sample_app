25879310654b955d70ec3bb0ebb64a6a1481070c
acgti <andy@hatchstud.io>
2015-07-01 00:23:10 +0000

    added full_title helper to remove home from title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Mandy"
+    assert_select "title", "Mandy"
   end
 
   test "should get help" do
