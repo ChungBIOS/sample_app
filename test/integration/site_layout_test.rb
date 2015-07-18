189f9b3b860a93eb4870ff21596beb4c7ed031b4
acgti <andy@hatchstud.io>
2015-07-18 23:36:11 +0000

    added test_helper to test titles

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,5 +11,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    get signup_path 
+    # selects the HTML element and then looks for method full_title that's in the 
+    assert_select "title", full_title('sign up')
+    get help_path
+    assert_select "title", full_title('Help')
   end
 end
