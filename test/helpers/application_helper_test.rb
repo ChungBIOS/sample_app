5b6a74c80aff9bfc62c001c0ffb5d40d4b77b0c0
acgti <andy@hatchstud.io>
2015-08-17 10:15:22 +0000

    added user and email validations

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
     assert_equal full_title,         "Mandy"
-    # assert_equal full_title("Help"), "Help | Mandy"
+    assert_equal full_title("Help"), "Help | Mandy"
     assert_equal full_title("sign up"), "sign up | Mandy"
   end
 end
