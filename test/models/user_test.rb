e0f3789e0cbda9123431aa1777d24be64a969520
acgti <andy@hatchstud.io>
2015-09-15 23:18:03 +0000

    added password validation to model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,8 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @user = User.new(name: "name", email: "name@example.com")
+    @user = User.new(name: "name", email: "name@example.com",
+                      password: "foobar", password_confirmation: "foobar")
   end
   
   test "test for valid user" do
@@ -31,18 +32,20 @@ class UserTest < ActiveSupport::TestCase
     @user.name = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
+
   
   test "email validation should accept valid addresses" do
-    valid_addresses = %w[user@example.com User@foo.com A_US-ER@foo.bar.org
-                          fitst.last@foo.jp alice+bob@baz.cn]
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                        first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
-  
+
+
   test "email validation should reject invalid addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. 
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                           foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
@@ -55,5 +58,16 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+  
+  test "password should be present (non-blank)" do
+    @user.password = @user.password_confirmation = " " *6
+    assert_not @user.valid?
+  end
+  
+  
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
